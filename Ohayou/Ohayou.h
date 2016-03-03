168dda8afed4bdfcd257cf91983a4caed3528471
bradhilton <brad.hilton.nw@gmail.com>
2016-03-03 08:02:40 -0700

    Initial Commit

---
+++ Ohayou/Ohayou.h
@@ -0,0 +1,19 @@
+//
+//  Ohayou.h
+//  Ohayou
+//
+//  Created by Bradley Hilton on 3/3/16.
+//  Copyright © 2016 Brad Hilton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Ohayou.
+FOUNDATION_EXPORT double OhayouVersionNumber;
+
+//! Project version string for Ohayou.
+FOUNDATION_EXPORT const unsigned char OhayouVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Ohayou/PublicHeader.h>
+
+
